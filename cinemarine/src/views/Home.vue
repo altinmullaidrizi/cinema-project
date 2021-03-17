9fd3b3c0d8c209218bb09a3cf3949b0fd00a0a2e
Blin <bk43730@ubt-uni.net>
2021-03-18 00:16:19 +0100

    Components Contact & Home

---
+++ cinemarine/src/views/Home.vue
@@ -0,0 +1,19 @@
+<template>
+  <div>
+    <Banner></Banner>
+    <HomeNavbar></HomeNavbar>
+    <OfertatComponent></OfertatComponent>
+  </div>
+</template>
+
+<script>
+// @ is an alias to /src
+import Banner from "./home/Banner";
+import HomeNavbar from "./home/HomeNavbar";
+import OfertatComponent from "./home/OfertatComponent";
+
+export default {
+  name: 'home',
+  components: {HomeNavbar, Banner, OfertatComponent},
+}
+</script>
