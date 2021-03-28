5738389f5ce524338ac3f2ecc768aac2b0baffe0
Altini <altin.m@vitamina-d.agency>
2021-03-28 23:48:51 +0200

    created admin components

---
+++ cinemarine/src/views/admin/Movies.vue
@@ -0,0 +1,17 @@
+<template>
+    <div>
+        <MovieTable></MovieTable>
+    </div>
+</template>
+
+<script>
+    import MovieTable from "./components/MovieTable";
+    export default {
+        name: "Movies",
+        components: {MovieTable},
+    }
+</script>
+
+<style scoped>
+
+</style>
